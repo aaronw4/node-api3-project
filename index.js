d8ba9912ceee109c030263f4250237b8bfb09d08
Aaron Williams <aaronrw4@yahoo.com>
2020-01-24 18:02:52 -0800

    fixed type

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
 
 server.listen(5000, () => {
-    console.log('Server running on http://localhost:5000');
+    console.log('Server running on http://localhost:5cons000');
 });
